e18c9b17aaf856d8f07e370c22b47f46f0c011b2
Solehjon Ruziboev <solehruziboev@gmail.com>
2018-12-20 19:47:49 +0300

    initializing LoadBalancer

---
+++ Application Level Routing 2/LoadBalancer.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class LoadBalancer {
+  public static void main(String[] args) throws IOException{
+    
+  }
+}
