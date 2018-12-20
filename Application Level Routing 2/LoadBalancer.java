a0303df9a163224334c669b1afb6dd98e255b16f
Solehjon Ruziboev <solehruziboev@gmail.com>
2018-12-20 20:04:46 +0300

    Getting operators in fomart

--- Application Level Routing 2/LoadBalancer.java
+++ Application Level Routing 2/LoadBalancer.java
@@ -4,9 +4,36 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class LoadBalancer {
-  public static void main(String[] args) throws IOException{
-    
-  }
+
+    static class Operator {
+        public String address;
+        public int port;
+        Operator(String address, int port) {
+            this.address = address;
+            this.port = port;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        String[] bindData = args[0].split(":");
+        String bindAddress = bindData[0];
+        int bindPort = Integer.parseInt(bindData[1]);
+
+        //System.out.println(bindAddress + " " + bindPort);
+        
+        ArrayList<Operator>operators = new ArrayList<Operator>();
+        for (int i = 1; i < args.length; i++){
+            String[] data = args[i].split(":");
+            String address = data[0];
+            int port = Integer.parseInt(data[1]);
+            operators.add(new Operator(address, port));
+        }
+
+        //for (int i = 0; i < operators.size(); i++)
+        //    System.out.println(operators.get(i).address + ":" + operators.get(i).port);
+    }
+
 }
